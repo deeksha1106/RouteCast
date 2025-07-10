5d31b125a9a7e8534bdba4c8638e7ee5de74a7b7
deeksha1106 <166601514+deeksha1106@users.noreply.github.com>
2025-07-10 15:27:07 +0530

    Update redis.js

--- api-server/redis.js
+++ api-server/redis.js
@@ -1,5 +1,16 @@
 const redis = require('redis');
-const client = redis.createClient();
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+  },
+});
+
+client.on('error', (err) => {
+  console.error('❌ Redis Client Error:', err);
+});
+
 client.connect();
 
 module.exports = client;
